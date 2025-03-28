57fbcc03ca102a411cf9ef41229112ebe5b50dbf
alessandrofalcoo <alefalco.1003@gmail.com>
2025-03-28 15:26:57 +0100

    implement fetchData function and update Main component layout; add Bootstrap styling

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,33 @@
 import { useState } from 'react'
+const api_endpoint = 'http://localhost:3000/posts?tags=Dolci'
+
 
 export default function Main() {
-    return (
-        <>
-            <main>
+    const [posts, setPosts] = useState([])
 
-            </main>
-        </>
-    )
+    function fetchData(url) {
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+
+            })
+    }
 }
+
+return (
+    <>
+        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-4 row-cols-lg-5 g-3">
+
+            <div className="container">
+                <div className="card h-100">
+                    <img className='card-img-top' src="" alt="" />
+                    <div className="card-body">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </>
+)
