02607a7c00f54a3f2169b7b557d7669439bc98df
alessandrofalcoo <alefalco.1003@gmail.com>
2025-03-28 16:23:14 +0100

    refactor Main component to improve data fetching and rendering of posts; enhance layout structure

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,41 +4,42 @@ const api_endpoint = 'http://localhost:3000/posts'
 
 export default function Main() {
     const [posts, setPosts] = useState([])
-    console.log(posts);
 
 
     useEffect(() => {
         fetchData(api_endpoint)
-    })
+    }, [])
 
 
     function fetchData(url) {
         fetch(url)
             .then(res => res.json())
             .then(data => {
-                console.log(data);
-                setPosts(data.title)
+                setPosts(data)
             })
     }
 
 
     return (
         <>
-            {
-                posts.map(post => (
-                    <div className="row row-cols-1 row-cols-sm-2 row-cols-md-4 row-cols-lg-5 g-3">
-                        <div className="container">
-                            <div className="card h-100">
-                                <img className='card-img-top' src={post.image} alt={post.slug} />
-                                <div className="card-body">
-                                    <h3>{post.title}</h3>
+            <div className="container">
+                {
+                    posts.map((post, index) => (
+                        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-4 row-cols-lg-5 g-3" key={index}>
+                            <div className="container" >
+                                <div className="card h-100" >
+                                    <img className='card-img-top' src={post.image} alt={post.slug} />
+                                    <div className="card-body">
+                                        <h3>{post.title}</h3>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
 
-                ))
-            }
+                    ))
+                }
+
+            </div>
 
         </>
     )
