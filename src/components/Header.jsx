b656ab63871dd8329c45290f17e8c69d7443e232
alessandrofalcoo <alefalco.1003@gmail.com>
2025-03-28 16:55:40 +0100

    refactor Header and Main components; update layout and styling, enhance image source handling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 export default function Header() {
     return (
-        <h1>My posts Blog</h1>
+        <h1 className="text-center mt-3">My posts Blog</h1>
     )
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-const api_endpoint = 'http://localhost:3000/posts'
+const api_endpoint = 'http://localhost:3000/posts/'
 
 
 export default function Main() {
@@ -15,6 +15,7 @@ export default function Main() {
         fetch(url)
             .then(res => res.json())
             .then(data => {
+                console.log(data);
                 setPosts(data)
             })
     }
@@ -22,23 +23,23 @@ export default function Main() {
 
     return (
         <>
-            <div className="container">
-                {
-                    posts.map((post, index) => (
-                        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-4 row-cols-lg-5 g-3" key={index}>
-                            <div className="container" >
+            <div className="container mt-4">
+                <div className="row row-cols-1 row-cols-sm-2 row-cols-md-4 row-cols-lg-5 g-3 mt-4">
+                    {
+                        posts.map((post, index) => (
+                            <div className="container" key={index}>
                                 <div className="card h-100" >
-                                    <img className='card-img-top' src={post.image} alt={post.slug} />
+                                    <img className='card-img-top' src={`http://localhost:3000/img/${post.image}`} alt={post.slug} />
                                     <div className="card-body">
                                         <h3>{post.title}</h3>
                                     </div>
                                 </div>
                             </div>
-                        </div>
 
-                    ))
-                }
+                        ))
+                    }
 
+                </div>
             </div>
 
         </>
